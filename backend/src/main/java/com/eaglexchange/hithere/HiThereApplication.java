9d3b8c5a871a102d8c3f3e19b3c94d0ec60300f0
Raghav <raghav2761@gmail.com>
2023-11-02 20:28:38 -0400

    intial backend commit

---
+++ backend/src/main/java/com/eaglexchange/hithere/HiThereApplication.java
@@ -0,0 +1,14 @@
+package com.eaglexchange.hithere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiThereApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiThereApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eaglexchange/hithere/HiThereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaglexchange.hithere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiThereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
