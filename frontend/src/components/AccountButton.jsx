129775fcbeb4d4f1ad504b93ef312490c46290ee
Raghav <raghav2761@gmail.com>
2023-11-29 19:24:38 -0500

    just a small change

--- frontend/src/components/AccountButton.jsx
+++ frontend/src/components/AccountButton.jsx
@@ -1,1 +1,14 @@
 // account button raghav
+import "./AccButton.css";
+import logo from "./3070444-200.png";
+function AccountButton() {
+  return (
+    <div class="container">
+      <div>
+        <img src={logo} alt="account button" width={50} height={50}></img>
+      </div>
+    </div>
+  );
+}
+
+export default AccountButton;
--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -1,1 +1,29 @@
-// Raghav Agarwalla 
+// Raghav Agarwalla
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+function ItemCard() {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title name </Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button>does this work</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ItemCard;
